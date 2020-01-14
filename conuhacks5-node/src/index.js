7d0f40847d2853e209cb9e2b91cd464f0c78088a
Laurendy Lam <l.lam@sap.com>
2020-01-14 10:43:43 -0500

    dockerize php and node apps

--- conuhacks5-node/src/index.js
+++ conuhacks5-node/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 3000;
+const port = 8080;
 const emojis = require('../emojis.json');
 
 /**
